a563b4d9f13b67b8bfbc87a03948c6ae6cc85d53
Tina2510 <tinachinivar@gmail.com>
2025-12-11 11:21:45 +0530

    created a contact class

---
+++ src/AddressBookMain.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class AddressBookMain {
+
+    static class Contact {
+        String  firstName, lastName,address, city, state, zip, phone, email;
+        Contact(String fn, String ln, String address, String city, String state,
+                String zip, String phone, String email) {
+            this.firstName = fn;
+            this.lastName = ln;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+        public static Contact createContact() {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("First Name: ");
+            String fn = sc.nextLine();
+
+            System.out.print("Last Name: ");
+            String ln = sc.nextLine();
+
+            System.out.print("Address: ");
+            String address = sc.nextLine();
+
+            System.out.print("City: ");
+            String city = sc.nextLine();
+
+            System.out.print("State: ");
+            String state = sc.nextLine();
+
+            System.out.print("ZIP: ");
+            String zip = sc.nextLine();
+
+            System.out.print("Phone: ");
+            String phone = sc.nextLine();
+
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+
+            return new Contact(fn, ln, address, city, state, zip, phone, email);
+        }
+    }
+}
